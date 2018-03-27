98e3003b782a4e2455b605d2784f9c4bd885e54f
Ana Rita Norinho <up201606003@fe.up.pt>
2018-03-27 12:10:43 +0100

    SINGINT_HANDLER done

--- simgrep.c
+++ simgrep.c
@@ -2,6 +2,7 @@
 #include <stdlib.h>
 #include <string.h>
 #include <strings.h>
+#include <signal.h>
 #define BUF_LENGTH 256
 #define _GNU_SOURCE
 char *filename;
@@ -12,10 +13,39 @@ int line_number=0;
 int total_lines=0;
 int word_compare=0;
 int directory=0;
+void clear (void)
+{
+  while ( getchar() != '\n' );
+}
 
 void verify_options();
-int main(int argc, char *argv[]){
+void sigint_handler(int signo)
+{
+	char input;
+	printf("Are you sure that you want to exit?\n");
 
+	input=getchar();
+	clear();
+
+	if (input=='Y' || input=='y'){
+		exit(0);
+	}
+	else if (input=='N' || input =='n')
+		return;
+	else
+		printf("Invalid input\n");
+}
+
+int main(int argc, char *argv[]){
+	struct sigaction action;
+	action.sa_handler = sigint_handler;
+	sigemptyset(&action.sa_mask);
+	action.sa_flags = 0;
+	if (sigaction(SIGINT,&action,NULL) < 0)
+	{
+		fprintf(stderr,"Unable to install SIGINT handler\n");
+		exit(1);
+	}
 	if (argc < 3) {
 		printf("Wrong number of arguments! Usage: %s [options] pattern [file/dir]\n", argv[0]);
 	}
@@ -100,6 +130,7 @@ void verify_options(){
 			}
 		}
 		i++;
+		sleep(10);
 	}
 	if (total_lines==1)
 	{
